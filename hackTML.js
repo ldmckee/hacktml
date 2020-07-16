3df863c102c39126f783d115e6034bd28dc7455b
ldmckee <61014747+ldmckee@users.noreply.github.com>
2020-07-15 21:08:44 -0400

    Add file via upload

---
+++ hackTML.js
@@ -0,0 +1,16 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+  let body = document.getElementsByTagName('body')[0]
+
+  body.innerHTML = body.innerHTML.replace(/<.+?>/, tag => {
+    let expandAttribute = name => text => {
+      return ' ' + name + '="' + text.slice(1, -1) + '"' + text.slice(-1)
+    }
+
+    tag = tag.replace(/#.+?[^\w^\-]/,  expandAttribute('id'))
+    tag = tag.replace(/\..+?[^\w^\-]/, expandAttribute('class'))
+
+    return tag
+  })
+})
