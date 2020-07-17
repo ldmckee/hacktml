a71d46dee8a04e7bab6c013c4b30bf69aaeca7af
Logan McKee <61014747+ldmckee@users.noreply.github.com>
2020-07-16 22:20:27 -0400

    Fix String.replace stopping after first match

--- hackTML.js
+++ hackTML.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   let body = document.getElementsByTagName('body')[0]
 
-  body.innerHTML = body.innerHTML.replace(/<.+?>/, tag => {
+  body.innerHTML = body.innerHTML.replace(/<.+?>/g, tag => {
     let expandAttribute = name => text => {
       return ' ' + name + '="' + text.slice(1, -1) + '"' + text.slice(-1)
     }
